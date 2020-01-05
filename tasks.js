1d7b1566dc63ce55d7b5cf5752b68be3ea541fcc
Ihor Dubas <ihordubas1525@gmail.com>
2020-01-05 21:15:07 +0200

    Add 5-10 tasks

--- tasks.js
+++ tasks.js
@@ -74,3 +74,66 @@ function removeItems(array) {
 }
 
 removeItems([3, 2, 5, 6, 7, 9])
+
+
+//  6. Increase the elements of array 1.5 times
+function increaseItems(array) {
+    if (!Array.isArray(array)) throw new Error('Provided not array')
+
+    let result = array.map(item => item * 1.5)
+    return result
+}
+
+increaseItems([1, 3, 5, 2, 4])
+
+
+
+//  7. Reverse array
+function reverseArr(array) {
+    if (!Array.isArray(array)) throw new Error('Provided not array')
+
+    let reversedArr = []
+    for( let i = array.length - 1; i >= 0; i--)
+        reversedArr.push(array[i])
+    return reversedArr
+}
+
+reverseArr([1, 2, 3, 4, 5])
+
+
+
+//  8. Increase the even indexes of the array by 2 times, decrease the odd indexes by 2.
+function changeItems(array) {
+    if (!Array.isArray(array)) throw new Error('Provided not array')
+
+    let result = array.map((item, i) => i % 2 == 0 ? item * 2 : item / 2)
+    return result
+}
+
+changeItems([1, 2, 3, 5, 4, 6])
+
+
+//  9. The product of all positive elements of an array.
+function productPositive(array) {
+    if (!array.length) return 0
+
+    let product = 1;
+    array.forEach(item => product *= item > 0 ? item : 1) 
+    return product
+}
+
+productPositive([1, 0, 3, 4, 5, -8])
+
+
+//  10. Palindrome
+function isPalindrome(str) {
+    if (typeof str !== 'string') throw new Error('Provided not string')
+
+    const palindrome = str.toLowerCase().split('').reverse().join('') 
+    return str.toLowerCase() == palindrome 
+}
+
+isPalindrome('Civic')
+
+
+//  11.
